a09482f5ff3488beeb6f60d6f3f041c98d3f471e
depontimatteo <37115585+depontimatteo@users.noreply.github.com>
2020-07-31 16:14:50 +0200

    daemonizing gunicorn

--- bin/docker_gunicorn_start.sh
+++ bin/docker_gunicorn_start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gunicorn --chdir app app:app -w 2 --threads 2 -b 0.0.0.0:5000 --log-level debug
+at now | gunicorn --chdir app app:app -w 2 --threads 2 -b 0.0.0.0:5000 --log-level debug --daemon
