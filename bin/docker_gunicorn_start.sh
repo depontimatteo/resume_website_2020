fe366ea7b884999183253cb3e8aaebc5741254c4
depontimatteo <37115585+depontimatteo@users.noreply.github.com>
2020-08-01 00:36:15 +0200

    added parametric education and experience + icons

--- bin/docker_gunicorn_start.sh
+++ bin/docker_gunicorn_start.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-gunicorn --chdir app app:app -w 2 --threads 2 -b 0.0.0.0:5000 --log-level debug --daemon
+DAEMON=$1
+
+if [ ${DAEMON} = "daemon"]
+then
+    gunicorn --chdir app app:app -w 2 --threads 2 -b 0.0.0.0:5000 --log-level debug --daemon
+else
+    gunicorn --chdir app app:app -w 2 --threads 2 -b 0.0.0.0:5000 --log-level debug
+fi
