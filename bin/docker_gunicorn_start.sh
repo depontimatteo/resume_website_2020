efb2efadab5cfd020699cfb26658a57290d07964
depontimatteo <37115585+depontimatteo@users.noreply.github.com>
2020-07-31 16:15:08 +0200

    typo

--- bin/docker_gunicorn_start.sh
+++ bin/docker_gunicorn_start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-at now | gunicorn --chdir app app:app -w 2 --threads 2 -b 0.0.0.0:5000 --log-level debug --daemon
+gunicorn --chdir app app:app -w 2 --threads 2 -b 0.0.0.0:5000 --log-level debug --daemon
