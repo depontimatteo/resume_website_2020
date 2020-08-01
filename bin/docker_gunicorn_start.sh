61098b9b163f0fca0302091a5264e52616666773
depontimatteo <37115585+depontimatteo@users.noreply.github.com>
2020-08-02 00:21:22 +0200

    fix deploy on heroku

--- bin/docker_gunicorn_start.sh
+++ bin/docker_gunicorn_start.sh
@@ -2,11 +2,10 @@
 
 DAEMON=$1
 
-nginx
-
 if [ ${DAEMON} = "daemon" ]
 then
     gunicorn --chdir app app:app -w 2 --threads 2 -b 0.0.0.0:5000 --log-level debug --daemon
 else
+    nginx
     gunicorn --chdir app app:app -w 2 --threads 2 -b 0.0.0.0:5000 --log-level debug
 fi
