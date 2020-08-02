691751a9e3311a1ed09e9d1faa1f8db60d0c0605
depontimatteo <37115585+depontimatteo@users.noreply.github.com>
2020-08-02 16:10:28 +0200

    added support dynamic port heroku

--- bin/docker_gunicorn_start.sh
+++ bin/docker_gunicorn_start.sh
@@ -4,8 +4,8 @@ DAEMON=$1
 
 if [ ${DAEMON} = "daemon" ]
 then
-    gunicorn --chdir app app:app -w 2 --threads 2 -b 0.0.0.0:5000 --log-level debug --daemon
+    gunicorn --chdir app app:app -w 2 --threads 2 -b 0.0.0.0:$PORT --log-level debug --daemon
 else
     nginx
-    gunicorn --chdir app app:app -w 2 --threads 2 -b 0.0.0.0:5000 --log-level debug
+    gunicorn --chdir app app:app -w 2 --threads 2 -b 0.0.0.0:$PORT --log-level debug
 fi
