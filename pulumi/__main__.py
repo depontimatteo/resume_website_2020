c8a7eabbaa4be99b82a5f46240acca627466b654
Matteo De Ponti <matteo.deponti@prima.it>
2022-04-18 18:17:36 +0200

    added IaC with pulumi, built eks-cluster on aws free tier

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,10 +1,39 @@
-"""An AWS Python Pulumi program"""
-
 import pulumi
-from pulumi_aws import s3
+import iam
+import vpc
+from pulumi_aws import eks
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('test-depo-pulumi')
+conf = pulumi.Config()
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+eks_cluster = eks.Cluster(
+    'eks-cluster',
+    name='eks-cluster',
+    role_arn=iam.eks_role.arn,
+    tags={
+        'Name': 'eks-cluster',
+    },
+    vpc_config=eks.ClusterVpcConfigArgs(
+        public_access_cidrs=['0.0.0.0/0'],
+        security_group_ids=[vpc.eks_security_group.id],
+        subnet_ids=vpc.subnet_ids,
+    ),
+)
+
+eks_node_group = eks.NodeGroup(
+    'eks-nodegroup',
+    cluster_name=eks_cluster.name,
+    node_group_name='eks-nodegroup',
+    node_role_arn=iam.ec2_role.arn,
+    instance_types=["t3.micro"],
+    subnet_ids=vpc.subnet_ids,
+    tags={
+        'Name': 'cluster-nodegroup',
+    },
+    scaling_config=eks.NodeGroupScalingConfigArgs(
+        desired_size=1,
+        max_size=2,
+        min_size=1,
+    ),
+)
+
+pulumi.export('cluster-name', eks_cluster.name)
